4881adc783b77b2d643e999136ebb23c4a10dcbd
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-07 17:51:19 -0300

    Fetch finalizado com os dados solicitados pelo atividade final, botão inserido jogando ja a pagina para a pagina clientes.html
    Acredito que falte dar uma estilizada ainda mas o cerne está pronto

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-function fetchApiData(){
+function fetchApiData() {
     fetch('https://randomuser.me/api/?results=5')
         .then(response => response.json())
         .then(data => {
-            for (i = 0; i <5; i++){
-                const div = document.createElement('div');
-                const img = document.createElement('img')
-                const f = document.createElement('p');
-                const w = document.createElement('p');
-                const c = document.createElement('p');
-                const d = document.createElement('p');
+            const divContainer = document.createElement('div');
+            divContainer.className = 'row row-cols-1 row-cols-md-6 g-6 container-fluid';
+            for (i = 0; i < 5; i++) {
+                const divColunas = document.createElement('div');
+                divColunas.className = 'col';
+                const divHeightCard = document.createElement('div');
+                divHeightCard.className = 'card h-100 d-flex flex-column';
+                const img = document.createElement('img');
+                img.className = 'card-img-top';
+                const divCardBody = document.createElement('div');
+                divCardBody.className = 'card-body';
+                const nameTitle = document.createElement('h5');
+                nameTitle.className = 'card-title';
+                const timeZoneDescription = document.createElement('p');
+                // const phone = document.createElement('p');
+                // const endereco = document.createElement('p');
+                const link = document.createElement('a');
+                link.className = 'link';
+                link.href = 'clientes.html';
+                link.setAttribute('target', '_blank');
+                const botaoEnviar = document.createElement('button');
+                botaoEnviar.className = 'btn btn-primary';
+
+
                 img.src = data.results[i].picture.large;
-                f.innerHTML = `Nome Completo: ` + data.results[i].name.title + " " + data.results[i].name.first + " " + data.results[i].name.last;
-                w.innerHTML = `e-mail: ` + data.results[i].email;
-                c.innerHTML = `Telefone: ` + data.results[i].phone;
-                d.innerHTML = `Cidade: ` + data.results[i].location.city + ', Estado: ' +data.results[i].location.state + ', País: ' + data.results[i].location.country;
-                
-                div.appendChild(img)
-                div.appendChild(f);
-                div.appendChild(w);
-                div.appendChild(c);
-                div.appendChild(d);
-                document.body.appendChild(div);
-                document.body.classList.add('card-body')
+                nameTitle.innerHTML = `Nome: ` + data.results[i].name.first + " " + data.results[i].name.last;
+                timeZoneDescription.innerHTML = `Fuso: ` + data.results[i].location.timezone.description;
+                botaoEnviar.innerHTML = `Enviar `;
+                // botaoEnviar.innerHTML = `Enviar`;
+                // phone.innerHTML = `Telefone: ` + data.results[i].phone;
+                // endereco.innerHTML = `Cidade: ` + data.results[i].location.city + ', Estado: ' + data.results[i].location.state + ', País: ' + data.results[i].location.country;
+
+                divContainer.appendChild(divColunas);
+                divColunas.appendChild(divHeightCard);
+                divHeightCard.appendChild(img);
+                divHeightCard.appendChild(divCardBody);
+                divCardBody.appendChild(nameTitle);
+                divCardBody.appendChild(timeZoneDescription);
+                divHeightCard.appendChild(link);
+                link.appendChild(botaoEnviar);
+                // divCardBody.appendChild(phone);
+                // divCardBody.appendChild(endereco);
+
+
             }
-            })
-        }
+            document.body.appendChild(divContainer);
+        })
+}
