19518b4d659a1d1dd0cdcf28c0291a625199c0f7
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-10 20:46:14 -0300

    dados de telefone e endereco nos cards

--- script.js
+++ script.js
@@ -20,22 +20,22 @@ function fetchApiData() {
                 const nameTitle = document.createElement('h5');
                 nameTitle.className = 'card-title';
                 const timeZoneDescription = document.createElement('p');
-                // const phone = document.createElement('p');
-                // const endereco = document.createElement('p');
+                 const phone = document.createElement('p');
+                const endereco = document.createElement('p');
                 const link = document.createElement('a');
                 link.className = 'link';
                 link.href = 'clientes.html';
                 link.setAttribute('target', '_blank');
                 const botaoEnviar = document.createElement('button');
+                
                 botaoEnviar.className = 'btn btn1';
 
                 img.src = data.results[i].picture.large;
                 nameTitle.innerHTML = `Nome: ` + data.results[i].name.first + " " + data.results[i].name.last;
                 timeZoneDescription.innerHTML = `Fuso: ` + data.results[i].location.timezone.description;
+                 phone.innerHTML = `Telefone: ` + data.results[i].phone;
+                 endereco.innerHTML = `Cidade: ` + data.results[i].location.city + ', Estado: ' + data.results[i].location.state + ', País: ' + data.results[i].location.country;
                 botaoEnviar.innerHTML = `Enviar `;
-                // botaoEnviar.innerHTML = `Enviar`;
-                // phone.innerHTML = `Telefone: ` + data.results[i].phone;
-                // endereco.innerHTML = `Cidade: ` + data.results[i].location.city + ', Estado: ' + data.results[i].location.state + ', País: ' + data.results[i].location.country;
 
                 
                 divContainer.appendChild(divColunas);
@@ -44,10 +44,10 @@ function fetchApiData() {
                 divHeightCard.appendChild(divCardBody);
                 divCardBody.appendChild(nameTitle);
                 divCardBody.appendChild(timeZoneDescription);
+                divCardBody.appendChild(phone);
+                divCardBody.appendChild(endereco);
                 divHeightCard.appendChild(link);
                 link.appendChild(botaoEnviar);
-                // divCardBody.appendChild(phone);
-                // divCardBody.appendChild(endereco);
 
 
             }
