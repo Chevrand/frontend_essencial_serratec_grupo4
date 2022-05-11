6c5c7235595e23ae33474f723e6ae70bdee62358
Sarah Almeida <sarahalmeida013@gmail.com>
2022-05-11 16:05:20 -0300

    testando paleta de cores

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
 window.onload = function () {
-    fetchApiData();
+    carregarDadosClientes();
 }
 
-function fetchApiData() {
+function carregarDadosClientes() {
     fetch('https://randomuser.me/api/?results=5')
-        .then(response => response.json())
-        .then(data => {
-            const divContainer = document.createElement('div');
-            divContainer.className = 'row row-cols-12 row-cols-sm-12 row-cols-md-6 g-lg-6 container-fluid';
-             for (i = 0; i < 5; i++) {
-                const divColunas = document.createElement('div');
-                divColunas.className = 'col';
-                const divHeightCard = document.createElement('div');
-                divHeightCard.className = 'card h-100 d-flex flex-column';
-                const img = document.createElement('img');
-                img.className = 'card-img-top';
-                const divCardBody = document.createElement('div');
-                divCardBody.className = 'card-body';
-                const nameTitle = document.createElement('h5');
-                nameTitle.className = 'card-title';
-                const timeZoneDescription = document.createElement('p');
-                 const phone = document.createElement('p');
-                const endereco = document.createElement('p');
-                const link = document.createElement('a');
-                link.className = 'link';
-                link.href = 'clientes.html';
-                link.setAttribute('target', '_blank');
-                const botaoEnviar = document.createElement('button');
-                
-                botaoEnviar.className = 'btn btn1';
+        .then(dadosClientes => dadosClientes.json())
+        .then(dadosClientes => dadosClientes.results)
+        .then(dadosClientes => construirCards(dadosClientes))
+}
 
-                img.src = data.results[i].picture.large;
-                nameTitle.innerHTML = `Nome: ` + data.results[i].name.first + " " + data.results[i].name.last;
-                timeZoneDescription.innerHTML = `Fuso: ` + data.results[i].location.timezone.description;
-                 phone.innerHTML = `Telefone: ` + data.results[i].phone;
-                 endereco.innerHTML = `Cidade: ` + data.results[i].location.city + ', Estado: ' + data.results[i].location.state + ', País: ' + data.results[i].location.country;
-                botaoEnviar.innerHTML = `Enviar `;
+function construirCards(clientes) {
+    const divContainer = document.createElement('div');
+    divContainer.className = 'row row-cols-12 row-cols-sm-12 row-cols-md-6 g-lg-6 container-fluid';
+    
+    for (i = 0; i < 5; i++) {
+    const divColunas = document.createElement('div');
+    const divHeightCard = document.createElement('div');
+    const img = document.createElement('img');
+    const divCardBody = document.createElement('div');
+    const nameTitle = document.createElement('h5');
+    const timeZoneDescription = document.createElement('p');
+    const phone = document.createElement('p');
+    const endereco = document.createElement('p');
+    const link = document.createElement('a');
+    const botaoEnviar = document.createElement('button');
 
-                
-                divContainer.appendChild(divColunas);
-                divColunas.appendChild(divHeightCard);
-                divHeightCard.appendChild(img);
-                divHeightCard.appendChild(divCardBody);
-                divCardBody.appendChild(nameTitle);
-                divCardBody.appendChild(timeZoneDescription);
-                divCardBody.appendChild(phone);
-                divCardBody.appendChild(endereco);
-                divHeightCard.appendChild(link);
-                link.appendChild(botaoEnviar);
+    divColunas.className = 'col';
+    divHeightCard.className = 'card h-100 d-flex flex-column';
+    divCardBody.className = 'card-body';
+    nameTitle.className = 'card-title';
+    img.className = 'card-img-top';
+    botaoEnviar.className = 'btn btn1';
+    link.className = 'link';
 
+    link.href = 'clientes.html';
+    link.setAttribute('target', '_blank');
+    
+    
+    img.src = clientes[i].picture.large;
+    timeZoneDescription.innerHTML = clientes[i].location.timezone.description;
+    nameTitle.innerHTML = clientes[i].name.first + " " + clientes[i].name.last;
+    botaoEnviar.innerHTML = `Enviar `;
+    // phone.innerHTML = `Telefone: ` + clientes[i].phone;
+    // endereco.innerHTML = `Cidade: ` + clientes[i].location.city + ', Estado: ' + clientes[i].location.state + ', País: ' + clientes[i].location.country;
 
-            }
-            document.getElementById('cardsAqui').appendChild(divContainer);
-            
-        })
+    divContainer.appendChild(divColunas);
+    divColunas.appendChild(divHeightCard);
+    divHeightCard.appendChild(img);
+    divHeightCard.appendChild(divCardBody);
+    divCardBody.appendChild(nameTitle);
+    divCardBody.appendChild(timeZoneDescription);
+    divCardBody.appendChild(phone);
+    divCardBody.appendChild(endereco);
+    divHeightCard.appendChild(link);
+    link.appendChild(botaoEnviar);
+
+    document.getElementById('cardsAqui').appendChild(divContainer);
+    }
 }
