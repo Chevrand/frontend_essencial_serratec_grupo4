dd4885265eb20ab34caf339e0c20d64c5a7673bd
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-12 15:55:08 -0300

    Fixed Dark Mode Button / Easter Egg Group 4
    Botão de mudança de tema agora altera também o seu texto, posicionamento para o canto superior direito da section do html, e implementação de easter eggs para os membros do grupo quando o site está em dark mode

--- Trabalho-Final-Frontend-Cooperativa-Amazonas/script.js
+++ Trabalho-Final-Frontend-Cooperativa-Amazonas/script.js
@@ -36,5 +36,15 @@ function fetchApiData() {
 
 function mudaTema() {
     document.body.classList.toggle("dark");
-    
-  }
+    mudaTextoBotaoTema();
+}
+
+function mudaTextoBotaoTema() {
+    var textoBotaoTema = document.getElementById("button-tema");
+    if (textoBotaoTema.innerHTML === "Dark Mode") {
+        textoBotaoTema.innerHTML = "Light Mode";
+    } else {
+        textoBotaoTema.innerHTML = "Dark Mode";
+        // alert('cuidado com os bugs atraídos pela luz')
+    }
+}
